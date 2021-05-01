675b308985dab645c92b2df0d43fd47d25168eb6
Samantha Papageorge <samantha_papageorge1@baylor.edu>
2021-04-30 22:24:30 -0500

    final commit

---
+++ MyShell.py
@@ -0,0 +1,33 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
+
+import django
+
+django.setup()
+
+from MainApp.models import Topic, Entry
+
+topics = Topic.objects.all()
+entries = Entry.objects.all()
+
+"""
+t = Topic.objects.get(id=1)
+print(t)
+entry = t.entry_set.all()
+print(entry)
+"""
+
+for t in topics:
+    print(f"Topic ID: {t.id} and Topic Name: {t}")
+    print(f"Date added: {t.date_added}")
+
+for e in entries:
+    print(f"Topic: {e.topic}")
+    print(f"Entry: {e.text}")
+
+
+from django.contrib.auth.models import User
+
+for user in User.objects.all():
+    print(user.username, user.id)
